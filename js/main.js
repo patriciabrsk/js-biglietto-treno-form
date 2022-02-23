53d40537b41ef69f5e264cdaec36c11fbe5637c3
patriciabrsk <patriciabresku@gmail.com>
2022-02-23 20:11:25 +0100

    input distance name instead of traveldistance

--- js/main.js
+++ js/main.js
@@ -2,20 +2,25 @@
 document.getElementById('submit').addEventListener('click', 
     // Collect data from input text form
     function() {
-        const inputName = document.getElementById('inputName').value;
+        const inputName = document.getElementById('input-name');
+        const name = inputName.value;
         // document.getElementById('user-name').innerHTML = inputName;
+        console.log(inputName);
 
-        const inputDistance = document.getElementById('inputDistance').value;
+        const inputDistance = document.getElementById('input-distance');
+        const km = inputDistance.value;
         console.log(inputDistance);
 
-        const inputAge = document.querySelector('#inputAge').value;
+        const inputAge = document.querySelector('#input-age');
+        const age = inputAge.value;
+        console.log(inputAge);
 
-        const standardPrice = (travelDistance * 0.27);
+        const standardPrice = (inputDistance * 0.27);
         let totalPrice;
 
-        if (inputAge < "under18") {
+        if (inputAge == "under18") {
             totalPrice = (standardPrice - (standardPrice * 0.17));
-        } else if (inputAge > "over65") {
+        } else if (inputAge == "over65") {
             totalPrice = (standardPrice - (standardPrice * 0.33));
         } else {
             totalPrice = standardPrice;
@@ -24,7 +29,7 @@ document.getElementById('submit').addEventListener('click',
         document.getElementById('user-name').innerHTML = inputName;
         document.getElementById('ticket-type').innerHTML = inputAge;
         document.getElementById('train-car').innerHTML = Math.floor(Math.random() * 10) + 1;
-        document.getElementById('cp-code').innerHTML = Math.floor(Math.random()*90000) + 10000;
+        document.getElementById('cp-code').innerHTML = Math.floor(Math.random() * 90000) + 10000;
         document.getElementById('price').innerHTML = `${totalPrice.toFixed(2)} &euro;`;
         document.getElementById('ticket').classList.remove("invisible");
     });
